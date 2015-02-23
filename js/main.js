deaad2f16a91ac775614da38da43f43cf4f7b145
Eric-Franklin79 <pickle797900@verizon.net>
2015-02-22 23:09:26 -0500

    night

--- js/main.js
+++ js/main.js
@@ -11,15 +11,19 @@ window.onload = function() {
        game.load.image('bloodbag', 'assets/bloodbag.png');
        game.load.image('cooler', 'assets/cooler.png');
        game.load.image('exit', 'assets/exit.png');
+       game.load.image('background', 'assets/background.png');
     }
     var bags;
     var cooler;
     var cursor;
     var blocktile
     var end;
-   
+    var bloodText;
+    var blood = 20.0;
+    var bloodTime
     
    function create() {
+   	   var background = game.add.sprite(641, 0, 'background');
    	   var map = game.add.tilemap('level1');
    	   map.addTilesetImage('hospitla-tileset', 'gameTiles');
    	   var backgroundLayer = map.createLayer('background');
@@ -35,6 +39,12 @@ window.onload = function() {
    	   cooler = game.add.sprite(60, 600, 'cooler');
    	   game.physics.arcade.enable(cooler);
    	   
+   	   var styleT = { font: "bold 25px Verdana", fill: "#C90000", align: "center" };
+    	   bloodText = game.add.text(645, 40, 'Blood: 100%', styleT);
+   	   
+    	   bloodTime = game.time.create(false);
+    	   bloodTime.add(20000, endLevel, this);
+    	   
    	   cursor = game.input.keyboard.createCursorKeys();
    }
     
@@ -47,24 +57,47 @@ window.onload = function() {
     	cooler.body.velocity.y = 0;
     	
     	if(cursor.left.isDown){
+    		if(!bloodTime.running){
+    			bloodTime.start();
+    		}
     		cooler.body.velocity.x = -64;	
     	}
     	else if(cursor.right.isDown){
+    		if(!bloodTime.running){
+    			bloodTime.start();
+    		}
     		cooler.body.velocity.x = 64;
     	}
     	if(cursor.up.isDown){
+    		if(!bloodTime.running){
+    			bloodTime.start();
+    		}
     		cooler.body.velocity.y = -64;
     	}
     	else if(cursor.down.isDown){
+    		if(!bloodTime.running){
+    			bloodTime.start();
+    		}
     		cooler.body.velocity.y = 64;
+    	}
+    	if(bloodTime.seconds < 20){
+    		bloodText.setText('Blood: ' + String(((20 - Math.floor(bloodTime.seconds))/20)*100) + '%');
+    	}
+    	else{
+    		bloodText.setText('Blood: 0%');
     	}
     }
     function drinkBlood(player, item){
     	    item.kill();
-    	    //increase blood level
+    	    bloodTime.destroy();
+    	    bloodTime = game.time.create(false);
+    	    bloodTime.add(20000, endLevel, this);
     }
     function exitToNext(player, exit){
     	    player.kill();
     	    //call function to change level
+    }
+    function endLevel(){
+    	    
     }
 };
